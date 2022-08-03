6c1ca94d876a444a189bcdb5c87b33cff3d721d7
Polina Sedunova <lina.malina.spb@gmail.com>
2022-08-03 18:59:19 +0300

    feat: add button and get-request

---
+++ src/components/Button.tsx
@@ -0,0 +1,16 @@
+import React from 'react';
+
+type ButtonPropsType={
+    nickName: string
+    callBack: ()=> void
+}
+
+export const Button = (props: ButtonPropsType) => {
+    const onClickHandler = () => {
+        props.callBack()
+    }
+
+    return (
+        <button onClick={onClickHandler}>{props.nickName}</button>
+    );
+};
